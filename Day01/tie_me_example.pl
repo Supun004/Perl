4d2384d9f3b934b9b2bea9fe34cfa820d9b7af51
ThilinaKariyawasam <thilina.kbgs@gmail.com>
2019-09-10 14:52:13 +0530

    added tie example

---
+++ Day01/tie_me_example.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+package MyArray;
+#tie with array
+
+sub TIEARRAY {
+    print "TYING\n";
+    bless [];
+}
+
+sub DESTROY{
+    print "DESTROYING\n";
+}
+
+sub STORE{
+    my ($self, $index, $value) = @_;
+    print "STORING $value at index $index\n";
+    $self[$index] = $value;
+}
+
+sub FETCH{
+    my ($self, $index) = @_;
+    print "FETCHING the value at index $index\n";
+    return $self[$index];
+}
+
+package main;
+$object = tie @x, MyArray; #@x is now a MyArray array;
+
+print "object is a ", ref($object), "\n";
+
+$x[0] = "This is test"; #this will call STORE();
+print $x[0], "\n"; #this will call FETCH();
+
+print $object->FETCH(0), "\n";
+
+#untie @x #now @x is a normal array again.
